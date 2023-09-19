39e7ed5742eb431b451e96e0961bd51c1e03e61c
jmarc0s <joaomarcoscacula88@gmail.com>
2023-09-19 16:21:57 -0300

    criando input de cor

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,6 +5,7 @@ const Time =  ({time, colaboradores, aoDeletar}) => {
     return (
         colaboradores.length > 0 ?
             <section className='time' style={{ backgroundColor: time.corSecundaria}}>
+                <input value={time.corSecundaria} type='color' className='input-cor' />
                 <h3 style={{borderColor: time.corPrimaria}}>{time.nome}</h3>
                 <div className='colaboradores'>
                     {colaboradores.map(colaborador => 
